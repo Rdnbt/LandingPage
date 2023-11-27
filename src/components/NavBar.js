99c7b08c5b35d8a528333515442e1a209bbb54a2
Rdnbt <haroldbudd12@gmail.com>
2023-11-27 17:49:18 +0900

    Deployment configuration

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,9 +37,9 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
-            <Link to="/">
+            <HashLink to="#home">
               <img src={logo} alt="Logo" className="logo" />
-            </Link>
+            </HashLink>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
